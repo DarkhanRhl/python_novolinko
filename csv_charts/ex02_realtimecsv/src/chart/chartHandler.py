f74a595386608f921fdc8a826dad9b1352996971
maxime <maxime.aiguier@epitech.eu>
2020-02-22 17:12:20 +0100

    ADD(dynamic plot creation)

---
+++ csv_charts/ex02_realtimecsv/src/chart/chartHandler.py
@@ -0,0 +1,60 @@
+import csv
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+from csvMyDialect import CsvMyDialect
+from csvHandler import CsvHandler
+import psutil
+import threading
+from computerMonitoring import ComputerMonitoring
+import time
+import datetime as dt
+import matplotlib.animation as animation
+
+class ChartHandler:
+
+    def __init__(self, name, nameLines):
+        self.fname = name
+        self.nameLines = nameLines
+        self.fig = go.Figure()
+        csv.register_dialect('my-dialect', CsvMyDialect())
+
+
+    def animate(self, i, xs, ys):
+        df = pd.read_csv(self.fname, dialect="my-dialect")     
+
+        vm = df['virtual-memory']
+        cpu = df['cpu']
+        xs.append(dt.datetime.now().strftime('%H:%M:%S'))
+        # xs.append(vm[len(vm) - 1])
+        ys.append(vm[len(cpu) - 1])
+
+        # Limit x and y lists to 50 items
+        xs = xs[-50:]
+        ys = ys[-50:]
+
+        # Draw x and y lists
+        self.ax.clear()
+        self.ax.plot(xs, ys)
+
+        # Format plot
+        plt.xticks(rotation=45, ha='right')
+        plt.subplots_adjust(bottom=0.30)
+        plt.title('Cpu consumption over Time')
+        plt.ylabel('Cpu')
+        time.sleep(2)
+
+        # Set up plot to call animate() function periodically
+
+    def handler(self):
+        self.cih = ComputerMonitoring()
+        self.csv = CsvHandler()
+        # self.animate(0, 0, 0)
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(1, 1, 1)
+        xs = []
+        ys = []
+        ani = animation.FuncAnimation(self.fig, self.animate, fargs=(xs, ys), interval=100)
+        plt.show()
+        
---
+++ csv_charts/ex02_realtimecsv/src/chart/csvHandler.py
@@ -0,0 +1,25 @@
+import csv
+from csvMyDialect import CsvMyDialect
+
+class CsvHandler:
+
+    def __init__(self):
+        csv.register_dialect('my-dialect', CsvMyDialect())
+
+    def write(self, file, toWrite):
+        file = open(file, "a")
+        writer = csv.writer(file, 'my-dialect')
+        writer.writerow(toWrite)
+        file.close()
+
+    def read(self, file):
+        file = open(file, "r")
+        reader = csv.reader(file)
+        for row in reader:
+            print(row)
+        file.close()
+
+    def createFile(self, file, rowTitle):
+        file = open(file, "w")
+        writer = csv.writer(file, 'my-dialect')
+        writer.writerow(rowTitle)
